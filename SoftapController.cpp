c39152652ad3ab9a61c34e44b0049bced73c5ed1
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2011-04-19 11:57:16 +0100

    Support LGE's version of the bcm4329 SoftAP
    LGE has a number of SDIO and power-management patches on top of
    bcm4329, which include separate pairs of functions to start/stop
    the driver in STA or AP mode. Use LGE's SOFTAP priv functions if
    WIFI_DRIVER_HAS_LGE_SOFTAP is true
    
    Change-Id: Id90a679794f12bfd3a35f9e0608288ae72cefd5f
    
    Conflicts:
    	SoftapController.cpp
    
    Signed-off-by: tonyp <tonyptonyp@gmail.com>

--- SoftapController.cpp
+++ SoftapController.cpp
@@ -132,7 +132,11 @@ int SoftapController::startDriver(char *iface) {
     }
 
     *mBuf = 0;
+#ifdef LGE_SOFTAP
+    ret = setCommand(iface, "START-SOFTAP");
+#else
     ret = setCommand(iface, "START");
+#endif
     if (ret < 0) {
         ALOGE("Softap driver start: %d", ret);
         return ret;
@@ -167,7 +171,11 @@ int SoftapController::stopDriver(char *iface) {
         ALOGE("Softap %s down: %d", iface, ret);
     }
 #endif
+#ifdef LGE_SOFTAP
+    ret = setCommand(iface, "STOP-SOFTAP");
+#else
     ret = setCommand(iface, "STOP");
+#endif
     ALOGD("Softap driver stop: %d", ret);
     return ret;
 }
