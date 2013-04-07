58465d9af69a2832a021307993099cdb4426dbe2
AndroidMeda <hephappy@gmail.com>
2013-04-07 18:07:04 +0300

    Revert "netd: Add support for BOARD_HAVE_BLUETOOTH_BLUEZ"
    This reverts commit 97c3cafff6db9e16999b32ec995215f2765daad2.

--- Android.mk
+++ Android.mk
@@ -6,16 +6,15 @@ LOCAL_SRC_FILES:=                                      \
                   BandwidthController.cpp              \
                   CommandListener.cpp                  \
                   DnsProxyListener.cpp                 \
-                  MDnsSdListener.cpp                   \
                   FirewallController.cpp               \
                   IdletimerController.cpp              \
                   InterfaceController.cpp              \
+                  MDnsSdListener.cpp                   \
                   NatController.cpp                    \
                   NetdCommand.cpp                      \
                   NetdConstants.cpp                    \
                   NetlinkHandler.cpp                   \
                   NetlinkManager.cpp                   \
-                  PanController.cpp                    \
                   PppController.cpp                    \
                   ResolverController.cpp               \
                   SecondaryTableController.cpp         \
@@ -29,8 +28,6 @@ LOCAL_SRC_FILES:=                                      \
 LOCAL_MODULE:= netd
 
 LOCAL_C_INCLUDES := $(KERNEL_HEADERS) \
-                    $(LOCAL_PATH)/../bluetooth/bluedroid/include \
-                    $(LOCAL_PATH)/../bluetooth/bluez-clean-headers \
                     external/mdnsresponder/mDNSShared \
                     external/openssl/include \
                     external/stlport/stlport \
@@ -61,7 +58,7 @@ ifneq ($(BOARD_HOSTAPD_DRIVER),)
   LOCAL_CFLAGS += -DHAVE_HOSTAPD
 endif
 
-ifeq ($(BOARD_HAVE_BLUETOOTH_BLUEZ),true)
+ifeq ($(BOARD_HAVE_BLUETOOTH),true)
   LOCAL_SHARED_LIBRARIES := $(LOCAL_SHARED_LIBRARIES) libbluedroid
   LOCAL_CFLAGS := $(LOCAL_CFLAGS) -DHAVE_BLUETOOTH
 endif
